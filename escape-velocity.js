ac06a01b14918c6ff0992a59fe154016f3baa989
Sorokopud Igor <venglovski@bk.ru>
2015-02-01 15:45:36 +0200

    Ready Header.

--- escape-velocity.js
+++ escape-velocity.js
@@ -1,3 +1,29 @@
-/**
- * Created by Home on 30.01.15.
- */
+jQuery(document).ready(function (){
+
+    /*menu*/
+
+    jQuery('.list').hide();
+    jQuery('li a[href=#active], .second,').hover(function () {
+        $('.second').show();
+    },function () {
+        jQuery('.second').hide();
+    });
+
+    jQuery('li a[href=#active2], .third').hover(function () {
+        $('.third, .second').show();
+    },function () {
+        jQuery('.third').hide();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
